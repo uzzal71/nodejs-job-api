480f1c357fcb78c80e4f08b02c7eea6de5f68673
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-27 00:49:14 +0600

    JWT Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,9 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
-
-    if (!name || !email || !password) {
-        throw new BadRequestError('Please provide name, email and password')
-    }
-    
-    const user = await User.create({ ...req.body });
-    res.status(StatusCodes.CREATED).json({ user })
+    const user = await User.create({ ...req.body })
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async(req, res) => {
