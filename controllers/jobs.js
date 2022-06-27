1d98931d20c0206d5b4c4323f93e9297ffccc5f1
uzzal71 <uzzalroy.acm@gmail.com>
2022-06-28 00:42:35 +0600

    Job Delete Added Job Controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -51,7 +51,19 @@ const updateJob = async(req, res) => {
 }
 
 const deleteJob = async(req, res) => {
-    res.send('delete job');
+    const {
+        user: { userId },
+        params: { id: jobId },
+      } = req
+    
+      const job = await Job.findByIdAndRemove({
+        _id: jobId,
+        createdBy: userId,
+      })
+      if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+      }
+      res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
